dd77eaa4c4384f0e39534736bf45c82301716530
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-02 12:51:04 +0200

    Dict merge funkar inte

--- Net-lab-4/Program.cs
+++ Net-lab-4/Program.cs
@@ -17,7 +17,17 @@ namespace Net_lab_4
             students.Add("Emilija", 3);
             students.Add("Petra", 2);
 
+            //create other dictionary and add to first
+            Dictionary<string, int> students2 = new Dictionary<string, int>();
+            students.Add("JonasA", 5);
+            students.Add("EmilijaB", 2);
+            students.Add("PetraC", 3);
+
+
             //
+          
+            Console.ReadLine();
+
 
         }
     }
