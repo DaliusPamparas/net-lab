0874a2ac2128dd3a7947ef0915ee44f24a54106e
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-02 13:15:13 +0200

    print dictionary

--- Net-lab-4/Program.cs
+++ Net-lab-4/Program.cs
@@ -13,19 +13,28 @@ namespace Net_lab_4
             //lab4
             //create ditionary and add values
             Dictionary<string, int> students = new Dictionary<string, int>();
-            students.Add("Jonas", 4);
-            students.Add("Emilija", 3);
-            students.Add("Petra", 2);
+            students.Add("Jonas", 40);
+            students.Add("Emilija", 30);
+            students.Add("Petra", 20);
+            students.Add("EmilijaA", 30);
+            students.Add("PetraV", 20);
+            students.Add("EmilijaJ", 30);
+            students.Add("Anna", 20);
 
             //create other dictionary and add to first
-            Dictionary<string, int> students2 = new Dictionary<string, int>();
-            students.Add("JonasA", 5);
-            students.Add("EmilijaB", 2);
-            students.Add("PetraC", 3);
+            //Dictionary<string, int> students2 = new Dictionary<string, int>();
+            //students.Add("JonasA", 50);
+            //students.Add("EmilijaB", 20);
+            //students.Add("PetraC", 30);
+
+            //print dictionary
+            foreach (KeyValuePair<string,int> item in students)
+            {
+                Console.WriteLine(item.Key + "                " + item.Value);
+            }
 
 
-            //
-          
+
             Console.ReadLine();
 
 
