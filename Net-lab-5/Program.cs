83938f73916fd20269303c0782405131a2ac5ff4
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-05 13:24:20 +0200

    5 done

--- Net-lab-5/Program.cs
+++ Net-lab-5/Program.cs
@@ -13,18 +13,26 @@ namespace Net_lab_5
             //create random and dictionary
             Random rndNumber = new Random();
             Dictionary<int, int> diceSpel = new Dictionary<int, int>();
+            diceSpel.Add(1, 0);
+            diceSpel.Add(2, 0);
+            diceSpel.Add(3, 0);
+            diceSpel.Add(4, 0);
+            diceSpel.Add(5, 0);
+            diceSpel.Add(6, 0);
 
             int rnd;
             for(int i=1; i<1000; i++)
             {
-                rnd = rndNumber.Next(1, 6);
-                if( diceSpel.ContainsValue(rnd))
-                    {
-                      
-                   }
+                rnd = rndNumber.Next(1, 7);
+               diceSpel[rnd] = diceSpel[rnd] + rnd;
+              
 
             }
-
+            foreach (KeyValuePair<int, int> item in diceSpel)
+            {
+                Console.WriteLine(item.Key + "                " + item.Value);
+            }
+            Console.ReadKey();
         }
     }
 }
