c8c4972b5eafc8fb17c7efad5606fffa2abf5185
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-05 10:21:47 +0200

    lab5

---
+++ Net-lab-5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create random and dictionary
+            Random rndNumber = new Random();
+            Dictionary<int, int> diceSpel = new Dictionary<int, int>();
+
+            int rnd;
+            for(int i=1; i<1000; i++)
+            {
+                rnd = rndNumber.Next(1, 6);
+                if( diceSpel.ContainsValue(rnd))
+                    {
+                      
+                   }
+
+            }
+
+        }
+    }
+}
