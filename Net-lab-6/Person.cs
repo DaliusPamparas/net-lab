5f243597f2eccd55ebdfacf8e4cc942394fd2b7a
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-06 09:54:44 +0200

    7 exercise

---
+++ Net-lab-6/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_lab_6
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+
+    }
+}
