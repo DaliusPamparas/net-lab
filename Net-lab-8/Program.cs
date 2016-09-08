8491aa0180616470e949cb838eaa69fbd67480f6
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-09-08 09:53:19 +0200

    fill list with random elements

--- Net-lab-8/Program.cs
+++ Net-lab-8/Program.cs
@@ -11,9 +11,29 @@ namespace Net_lab_8
         static void Main(string[] args)
         {
             List<int> myCollection = new List<int>();
-            Fill fill = new Fill();
-            
+            FillCollection( myCollection);
+
+            foreach(int i in myCollection)
+            {
+               Console.WriteLine(i);
+            }
+            Console.ReadKey();
 
         }
+            public static void FillCollection(List<int> myCollection)
+              {
+            Random ran = new Random();
+            for( int i = 0; i < 200; i++)
+            {
+                int tal = ran.Next(1, 1000);
+                while( myCollection.Contains(tal))
+                {
+                    tal = ran.Next(1, 100);
+
+                }
+                myCollection.Add(tal);
+            }
+              }
+        }
     }
-}
+
